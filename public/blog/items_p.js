ac8e22fb40f356a37b4630e06f7e0bedbbda110b
wgehner <wgehner@gmail.com>
2018-04-11 12:20:21 -0700

    Rename

--- public/blog/items_p.js
+++ public/blog/items_p.js
@@ -1,5 +1,5 @@
 function pug_escape(e){var a=""+e,t=pug_match_html.exec(a);if(!t)return e;var r,c,n,s="";for(r=t.index,c=0;r<a.length;r++){switch(a.charCodeAt(r)){case 34:n="&quot;";break;case 38:n="&amp;";break;case 60:n="&lt;";break;case 62:n="&gt;";break;default:continue}c!==r&&(s+=a.substring(c,r)),c=r+1,s+=n}return c!==r?s+a.substring(c,r):s}
-var pug_match_html=/["&<>]/;function itemsTagTemplate(locals) {var pug_html = "", pug_mixins = {}, pug_interp;;var locals_for_with = (locals || {});(function (items) {// iterate items
+var pug_match_html=/["&<>]/;function itemsPTemplate(locals) {var pug_html = "", pug_mixins = {}, pug_interp;;var locals_for_with = (locals || {});(function (items) {// iterate items
 ;(function(){
   var $$obj = items;
   if ('number' == typeof $$obj.length) {
@@ -17,4 +17,4 @@ pug_html = pug_html + "\u003Ca href=\"#{item.local_url}\" target=\"_blank\"\u003
   }
 }).call(this);
 
-pug_html = pug_html + "\u003Cp\u003E\u003C\u002Fp\u003E\u003Cp\u003E\u003C\u002Fp\u003E\u003Cscript\u003Efunction myfunc(){\n\tconsole.log('myfunc')\n}\n\u003C\u002Fscript\u003E";}.call(this,"items" in locals_for_with?locals_for_with.items:typeof items!=="undefined"?items:undefined));;return pug_html;}
+pug_html = pug_html + "\u003Cp\u003E\u003C\u002Fp\u003E\u003Cp\u003E\u003C\u002Fp\u003E\u003Cscript\u003Efunction myfunc(){\n\tconsole.log('myfunc')\n}\u003C\u002Fscript\u003E";}.call(this,"items" in locals_for_with?locals_for_with.items:typeof items!=="undefined"?items:undefined));;return pug_html;}
